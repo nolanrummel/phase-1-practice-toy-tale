908744a047109a7c0a13c5a8c46c1dbb7e4ef3f1
Nolan Rummel <nolan.rummel22@gmail.com>
2023-05-30 11:56:23 -0400

    reading image, header and likes from API' git push ls '

--- src/index.js
+++ src/index.js
@@ -13,3 +13,39 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function fetcher(){
+  fetch("http://localhost:3000/toys")
+  .then((r) => r.json())
+  .then(toys => {
+    toys.forEach(toy => toyCreator(toy))
+  })
+}
+
+const toyCollection = document.querySelector('#toy-collection')
+
+function toyCreator(toy){
+  const toyDiv = document.createElement('div')
+  toyDiv.classList.add('card')
+
+  const toyHeader = document.createElement('h2')
+  toyHeader.textContent = toy.name
+
+  const toyImage = document.createElement('img')
+  toyImage.src = toy.image
+  toyImage.classList = ('toy-avatar')
+
+  const toyLikes = document.createElement('p')
+  toyLikes.textContent = toy.likes
+  toyLikes.
+  
+    toyDiv.append(toyHeader)
+    toyDiv.append(toyImage)
+    toyDiv.append(toyLikes)
+      toyCollection.appendChild(toyDiv)
+  }
+
+
+
+fetcher()
+// toyCreator(toy)
